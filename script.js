beee13b05dad8240a7245b01aa931c2fa86af0c6
michael Zuniga <michaelzunigaa@gmail.com>
2020-03-20 12:55:12 -0700

    script-file

---
+++ script.js
@@ -0,0 +1,8 @@
+var searchTerms =$("#search-term");
+var numberRec =$("#number-records");
+
+var  startYear =$("#start-year");
+var endYeat =$("#end-year");
+
+var search =$("src-btn");
+var clear =$("clear-btn");
