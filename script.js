5ebba233d4c0d33d798a50c77ca6aca109a3377d
bbtwots <brettbelka@gmail.com>
2020-03-20 13:44:56 -0700

    ajax working

--- script.js
+++ script.js
@@ -1,31 +1,28 @@
-var searchTerms =$("#search-term");
+var searchTerms = $("#search-term");
+var numberRec = $("#number-records");
+var startYear = $("#start-year");
+var endYear = $("#end-year");
+var search = $("#src-btn");
+var clear = $("#clear-btn");
+var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=AexBs82F271plW5NYllpAgocdxd0Q2Dw"
 
-var numberRec =$("#number-records");
+// $(search).click(function(){
 
-
-
-var  startYear =$("#start-year");
-
-var endYear =$("#end-year");
-
-var search
-
-
-var search =$("src-btn");
-var clear =$("clear-btn");
-
-
-$(search).click(function(){
-    q=*topic you want*&facet_fields=source&facet=true&begin_date=YYYYMMDD&end_date=YYYYMMDD
+// });
 
 
 
-});
+// $(clear).click(function() {
+//      $("input").empty();
 
+//   });
+function searchFunc() { 
+    console.log("HI");
+$.ajax({
+    url: queryURL,
+    method: "GET"
+}).then(function (response) {
+    console.log(response);
+});};
 
-
-$(clear).click(function() {
-     $("input").empty();
-
-  });
-
+searchFunc();
